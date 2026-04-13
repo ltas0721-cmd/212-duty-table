adabe35686c4a80494f4ce41c18b56aacd698b36
ltas0721-cmd <ltas0721@gmail.com>
2026-04-13 18:53:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import datetime
 st.title("🗑️ 212宿舍倒垃圾排班表")
 
 def get_duty_person():
-    # 固定的轮换顺序
     roommates = ["李名伟", "王晟", "郑加裕", "陈镇灿"]
     num_people = len(roommates)
 
@@ -13,20 +12,19 @@ def get_duty_person():
     # 1. 修改为回宿舍、重新开始排班的那一天
     anchor_date = datetime.date(2026, 5, 5) # 假设五一假期5月5日回来
     
-    # 2. 修改回宿舍这一天，决定由谁先开始倒垃圾（必须是上面四个名字之一）
+    # 2. 修改回宿舍这一天，决定由谁先开始倒垃圾
     anchor_person = "郑加裕" 
     
-    # ================================================
     
     # 获取这个基准人在列表里的初始位置 (0, 1, 2, 3)
     anchor_index = roommates.index(anchor_person)
     
     selected_date = st.date_input("请选择你想查看的日期：", datetime.date.today())
     
-    # 计算经过的天数（过去的日子会变成负数，不影响排班逻辑）
+    # 计算经过的天数
     days_passed = (selected_date - anchor_date).days
     
-    # 核心算法升级：加上了“锚点人”的偏移量
+    # 加上“锚点人”的偏移量
     today_index = (days_passed + anchor_index) % num_people
     tomorrow_index = (days_passed + 1 + anchor_index) % num_people
     
