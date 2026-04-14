1b568205f50df3e9a04f4784c1a3d5cd0fc5a477
Wakaba_Mutsumi <ltas0721@gmail.com>
2026-04-14 20:47:48 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -79,7 +79,7 @@ def main():
 ---
 🔜 明天准备接客的是：【{tomorrow_person}】<br>
 
-<font color="#808080" size="2">*(本通知由宿舍云端物理超度系统 2.0 自动发送)*</font>
+<font color="#808080" size="2">*(本通知由宿舍云端物理超度系统 3.0 自动发送)*</font>
 """
     
     execute_pushplus_notice(push_token, push_topic, title, content)
